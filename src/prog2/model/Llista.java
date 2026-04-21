289a8c4c0e5a0628744d6e662fbec689b93c537d
sergi <sasencji83@alumnes.ub.edu>
2026-04-21 19:13:37 +0200

    Todo Subido

--- src/prog2/model/Llista.java
+++ src/prog2/model/Llista.java
@@ -1,68 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prog2.model;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import prog2.vista.BiblioException;
 
-public class Llista<T> implements Serializable {
-   protected ArrayList<T> llista;
+import java.util.ArrayList;
+import java.io.Serializable;
+import prog2.vista.BiblioException;
 
-   public Llista() {
-       llista = new ArrayList<>();
+
+
+public class Llista<T> implements InLlista,Serializable{
+    protected ArrayList<T> llista;
+
+    public Llista() {
+        llista = new ArrayList<>();
     }
 
     /**
      * Retornar nombre d'elements continguts a la llista
      */
+    @Override
     public int getSize() {
-          // TO-BE-DONE
+        return 0;
     }
 
     /**
      * Afegir element a la llista. Afegeix l'element t a la llista
+     *
+     * @param o
      */
-    public void afegir(T t) throws BiblioException {
-          // TO-BE-DONE
+    @Override
+    public void afegir(Object o) throws BiblioException {
+
     }
 
     /**
      * Esborrar element de la llista. Esborra l'element t a la llista
+     *
+     * @param o
      */
-    public void esborrar(T t) {
-          // TO-BE-DONE
+    @Override
+    public void esborrar(Object o) {
+
     }
 
     /**
      * Retornar element de la llista a la posició position
+     *
+     * @param position
      */
-    public T getAt(int position) {
-          // TO-BE-DONE
+    @Override
+    public Object getAt(int position) {
+        return null;
     }
 
     /**
      * Buidar tots el elements de la llista
      */
+    @Override
     public void clear() {
-          // TO-BE-DONE
+
     }
 
     /**
      * Retornar true si la llista és buida
      */
+    @Override
     public boolean isEmpty() {
-          // TO-BE-DONE
+        return false;
     }
 
     /**
      * Retornar l'ArrayList que es fa servir dins de la classe
      */
-    public ArrayList<T> getArrayList() {
-        ArrayList<T> arrlist = new ArrayList<>(llista);
-        return arrlist;
+    @Override
+    public ArrayList getArrayList() {
+        return null;
     }
 }
