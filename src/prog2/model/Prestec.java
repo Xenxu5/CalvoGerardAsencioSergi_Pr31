e2f00a8894339631e2a8f2359773a72ad2919071
sergi <sasencji83@alumnes.ub.edu>
2026-05-09 13:29:34 +0200

    Model done

--- src/prog2/model/Prestec.java
+++ src/prog2/model/Prestec.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public abstract class Prestec implements InPrestec{
 
     /**
-     * Atributs privats de Prestec
+     * Atributs privats de Preste
      */
     private Exemplar exemplar;
     private Usuari usuari;
