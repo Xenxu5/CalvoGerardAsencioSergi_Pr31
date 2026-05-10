78ace38b31229226222e62d1a5b2a2ca6c210e0a
gerry11cm <gcalvomartos@gmail.com>
2026-05-10 12:33:32 +0200

    He possat serializable ha totes les classes i he començat Adaptador. Avisa si haces push ratilla ;(

--- src/prog2/model/Prestec.java
+++ src/prog2/model/Prestec.java
@@ -2,9 +2,10 @@ package prog2.model;
 
 import prog2.vista.BiblioException;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public abstract class Prestec implements InPrestec{
+public abstract class Prestec implements InPrestec, Serializable {
 
     /**
      * Atributs privats de Prestec
