a3a9a627cb00940303ff13c9151088c76942cbad
sergi <sasencji83@alumnes.ub.edu>
2026-05-18 21:17:14 +0200

    Empezamos

---
+++ src/prog2/vista/sa.java
@@ -0,0 +1,11 @@
+package prog2.vista;
+
+import javax.swing.*;
+
+public class sa {
+    private JButton button1;
+    private JPanel panel1;
+    private JButton button2;
+    private JButton button3;
+    private JLabel Texto;
+}
